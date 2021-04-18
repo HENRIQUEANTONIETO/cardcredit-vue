1b22ec696df8827539c1b243dce14a4b18c55899
Henrique Antonieto <henriquebedendo10@live.com>
2021-04-18 20:39:44 -0300

    add file server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const serveStatic = require('serve-static')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
+const port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
