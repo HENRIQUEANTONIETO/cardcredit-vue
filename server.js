a1805489e3150dec7a5d542167312c3c3bb8eb2e
Henrique Antonieto <henriquebedendo10@live.com>
2021-04-18 21:32:23 -0300

    Add cool face API again

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ const app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 const port = process.env.PORT || 5000
+app.get('/cool', (req, res) => res.send(cool()))
 app.listen(port)
 console.log('server started ' + port)
